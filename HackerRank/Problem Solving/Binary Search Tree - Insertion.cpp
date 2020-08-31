867755f2cb3aec048251f68072606b98ab2704fe
ngandhi369 <gandhinirdosh@gmail.com>
2020-08-31 22:34:06 +0530

    added

---
+++ HackerRank/Problem Solving/Binary Search Tree - Insertion.cpp
@@ -0,0 +1,34 @@
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+Node* insert(Node* root, int data)
+{
+    if(root == NULL)
+    {
+        root = new Node(data);
+    }
+    else if(data < root->data)
+        root->left = insert(root->left, data);
+    else if(data > root->data)
+        root->right = insert(root->right, data);
+    return root;
+}
+
+
+
