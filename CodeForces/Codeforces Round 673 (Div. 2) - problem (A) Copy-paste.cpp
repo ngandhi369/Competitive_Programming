4ff78b65ea62e4421e415a4810195487e6bceb12
ngandhi369 <gandhinirdosh@gmail.com>
2020-10-05 12:00:32 +0530

    Added till round 673

---
+++ CodeForces/Codeforces Round 673 (Div. 2) - problem (A) Copy-paste.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t,n,k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+ 
+        sort(a,a+n);
+ 
+        int sum=0;
+        for(int i=1;i<n;i++)
+        {
+            sum=sum+(k-a[i])/a[0];
+        }
+        cout<<sum<<endl;
+    }
+ 
+return 0;
+}
